63045ac41ec9ef7cd89741349e36c3c5ac8398c6
ElizabethHuang <lizne6z0@gmail.com>
2025-06-21 11:47:04 +0800

    feat: Get JWT token from cookie when checking the token.

--- middleware/auth.go
+++ middleware/auth.go
@@ -3,23 +3,22 @@ package middleware
 import (
 	"net/http"
 	"show-calendar/utils"
-	"strings"
 
 	"github.com/gin-gonic/gin"
 )
 
 func AuthRequired() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		token := c.Request.Header.Get("Authorization")
-		if len(token) <= 7 && !strings.HasPrefix(token, "Bearer") {
+		token, err := c.Cookie("authorization")
+		if err != nil {
 			c.JSON(http.StatusUnauthorized, gin.H{
-				"message": "token invalid",
+				"message": "token not found",
 			})
 			c.Abort()
 			return
 		}
 
-		claims, err := (&utils.Jwt{}).ParseUserToken(token[7:])
+		claims, err := (&utils.Jwt{}).ParseUserToken(token)
 		if err != nil {
 			c.JSON(http.StatusUnauthorized, gin.H{
 				"message": "token invalid",
